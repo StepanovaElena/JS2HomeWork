c98fbb5da1b9371e0bc0bcdbcc03706a5e30a0dd
StepanovaElena <stepanova_eg@bk.ru>
2018-09-20 20:58:11 +0300

    Задание 3 к уроку номер 6

---
+++ 6(3)/js.js
@@ -0,0 +1,10 @@
+"use strict";
+
+    $(document).ready(function(){
+        $('.general-product-block').slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+    })
+    });
